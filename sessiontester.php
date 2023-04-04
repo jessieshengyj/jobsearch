2787a8806dd3d5451e9228958f34ac131274f5bd
devdev <devdevin007@gmail.com>
2023-04-03 20:10:18 -0700

    added session tester

---
+++ sessiontester.php
@@ -0,0 +1,312 @@
+<!--Test Oracle file for UBC CPSC304 2018 Winter Term 1
+  Created by Jiemin Zhang
+  Modified by Simona Radu
+  Modified by Jessica Wong (2018-06-22)
+  This file shows the very basics of how to execute PHP commands
+  on Oracle.
+  Specifically, it will drop a table, create a table, insert values
+  update values, and then query for values
+
+  IF YOU HAVE A TABLE CALLED "demoTable" IT WILL BE DESTROYED
+
+  The script assumes you already have a server set up
+  All OCI commands are commands to the Oracle libraries
+  To get the file to work, you must place it somewhere where your
+  Apache server can run it, and you must rename it to have a ".php"
+  extension.  You must also change the username and password on the
+  OCILogon below to be your ORACLE username and password -->
+
+  <html>
+    <head>
+        <title>CPSC 304 PHP/Oracle Demonstration</title>
+    </head>
+
+    <body>
+        <h2>Reset</h2>
+        <p>If you wish to reset the table press on the reset button. If this is the first time you're running this page, you MUST use reset</p>
+
+        <form method="POST" action="backend.php">
+            <!-- if you want another page to load after the button is clicked, you have to specify that page in the action parameter -->
+            <input type="hidden" id="resetTablesRequest" name="resetTablesRequest">
+            <p><input type="submit" value="Reset" name="reset"></p>
+        </form>
+       
+
+         <hr />
+        <h2>Create a Company</h2>
+
+        <form method="POST" action="backend.php"> <!--refresh page when submitted-->
+            <input type="hidden" id="insertCompanyRequest" name="insertCompanyRequest">
+            CompanyID (number): <input type="text" name="InsCompanyCompanID"> <br /><br />
+            Company Name: <input type="text" name="InsCompanyName"> <br /><br />
+            NumberOfEmployees: <input type="text" name="InsCompanyNumberOfEmployees"> <br /><br />
+            <input type="submit" value="Insert" name="insertSubmit"></p>
+        </form>
+
+        <hr />
+
+        <h2>Delete a Company</h2>
+
+        <form method="POST" action="backend.php"> <!--refresh page when submitted-->
+            <input type="hidden" id="deleteCompanyRequest" name="deleteCompanyRequest">
+            CompanyID (number): <input type="text" name="DelCompanyCompanID"> <br /><br />
+            <input type="submit" value="Delete" name="DeleteSubmit"></p>
+        </form>
+
+        <hr />
+
+        <h2>Count the Tuples in DemoTable</h2>
+        <form method="GET" action="backend.php"> <!--refresh page when submitted-->
+            <input type="hidden" id="countTupleRequest" name="countTupleRequest">
+            <input type="submit" name="countTuples"></p>
+        </form>
+
+    <hr />
+
+    <h2>Create a Job</h2>
+
+    <form method="POST" action="backend.php"> <!--refresh page when submitted-->
+        <input type="hidden" id="insertJobRequest" name="insertJobRequest">
+        JobID (Number): <input type="text" name="InsJobID"> <br /><br />
+        Application Deadline (yyyy--mm--dd): <input type="text" name="InsJobDeadline"> <br /><br />
+        Remote (yes, no, or hybrid): <input type="text" name="InsJobRemote"> <br /><br />
+        PositionName: <input type="text" name="InsJobPosName"> <br /><br />
+        StartDate (yyyy--mm--dd): <input type="text" name="InsJobStartDate"> <br /><br />
+        CompanyID (Number) : <input type="text" name="InsJobCompID"> <br /><br />
+        Job Catagory: <input type="text" name="InsJobCatagory"> <br /><br />
+        <input type="submit" value="Insert" name="insertSubmit"></p>
+    </form>
+
+  <hr />
+
+
+        <?php
+		//this tells the system that it's no longer just parsing html; it's now parsing PHP
+
+        $success = True; //keep track of errors so it redirects the page only if there are no errors
+        $db_conn = NULL; // edit the login credentials in connectToDB()
+        $show_debug_alert_messages = False; // set to True if you want alerts to show you which methods are being triggered (see how it is used in debugAlertMessage())
+
+        function debugAlertMessage($message) {
+            global $show_debug_alert_messages;
+
+            if ($show_debug_alert_messages) {
+                echo "<script type='text/javascript'>alert('" . $message . "');</script>";
+            }
+        }
+
+        function executePlainSQL($cmdstr) { //takes a plain (no bound variables) SQL command and executes it
+            //echo "<br>running ".$cmdstr."<br>";
+            global $db_conn, $success;
+
+            $statement = OCIParse($db_conn, $cmdstr);
+            //There are a set of comments at the end of the file that describe some of the OCI specific functions and how they work
+
+            if (!$statement) {
+                echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+                $e = OCI_Error($db_conn); // For OCIParse errors pass the connection handle
+                echo htmlentities($e['message']);
+                $success = False;
+            }
+
+            $r = OCIExecute($statement, OCI_DEFAULT);
+            if (!$r) {
+                echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+                $e = oci_error($statement); // For OCIExecute errors pass the statementhandle
+                echo htmlentities($e['message']);
+                $success = False;
+            }
+
+			return $statement;
+		}
+
+        function executeBoundSQL($cmdstr, $list) {
+            /* Sometimes the same statement will be executed several times with different values for the variables involved in the query.
+		In this case you don't need to create the statement several times. Bound variables cause a statement to only be
+		parsed once and you can reuse the statement. This is also very useful in protecting against SQL injection.
+		See the sample code below for how this function is used */
+
+			global $db_conn, $success;
+			$statement = OCIParse($db_conn, $cmdstr);
+
+            if (!$statement) {
+                echo "<br>Cannot parse the following command: " . $cmdstr . "<br>";
+                $e = OCI_Error($db_conn);
+                echo htmlentities($e['message']);
+                $success = False;
+            }
+
+            foreach ($list as $tuple) {
+                foreach ($tuple as $bind => $val) {
+                    //echo $val;
+                    //echo "<br>".$bind."<br>";
+                    OCIBindByName($statement, $bind, $val);
+                    unset ($val); //make sure you do not remove this. Otherwise $val will remain in an array object wrapper which will not be recognized by Oracle as a proper datatype
+				}
+
+                $r = OCIExecute($statement, OCI_DEFAULT);
+                if (!$r) {
+                    echo "<br>Cannot execute the following command: " . $cmdstr . "<br>";
+                    $e = OCI_Error($statement); // For OCIExecute errors, pass the statementhandle
+                    echo htmlentities($e['message']);
+                    echo "<br>";
+                    $success = False;
+                }
+            }
+        }
+
+
+        function connectToDB() {
+            global $db_conn;
+
+            // Your username is ora_(CWL_ID) and the password is a(student number). For example,
+			// ora_platypus is the username and a12345678 is the password.
+            $db_conn = OCILogon("ora_devr07", "a80666621", "dbhost.students.cs.ubc.ca:1522/stu");
+
+            if ($db_conn) {
+                debugAlertMessage("Database is Connected");
+                return true;
+            } else {
+                debugAlertMessage("Cannot connect to Database");
+                $e = OCI_Error(); // For OCILogon errors pass no handle
+                echo htmlentities($e['message']);
+                return false;
+            }
+        }
+
+        function disconnectFromDB() {
+            global $db_conn;
+
+            debugAlertMessage("Disconnect from Database");
+            OCILogoff($db_conn);
+        }
+
+        function handleUpdateApplicantRequest() {
+            global $db_conn;
+            $tuple = array (
+                ":bind1" => $_POST['UpdUserID'],
+                ":bind2" => $_POST['UpdEmail'],
+                ":bind3" => $_POST['UpdName'],
+                ":bind4" => $_POST['UpdAddress'],
+                ":bind5" => $_POST['UpdPhoneNumber']
+            );
+
+            $alltuples = array (
+                $tuple
+            );
+           
+            executeBoundSQL("update Applicant set Email = :bind2, Name = :bind3, Addy = :bind4, PhoneNumber = :bind5 where UserID = :bind1", $alltuples);
+            OCICommit($db_conn);
+        }
+
+        function handleResetRequest() {
+            global $db_conn;
+            // Drop old table
+            executePlainSQL("DROP TABLE demoTable");
+
+            // Create new table
+            echo "<br> creating new table <br>";
+            executePlainSQL("CREATE TABLE demoTable (id int PRIMARY KEY, name char(30))");
+            OCICommit($db_conn);
+        }
+
+        function handleInsertApplicantRequest() {
+            global $db_conn;
+
+            //Getting the values from user and insert data into the table
+            $tuple = array (
+                ":bind1" => $_POST['InsUserID'],
+                ":bind2" => $_POST['InsEmail'],
+                ":bind3" => $_POST['InsName'],
+                ":bind4" => $_POST['InsAddress'],
+                ":bind5" => $_POST['InsPhoneNumber']
+            );
+
+            $alltuples = array (
+                $tuple
+            );
+
+            executeBoundSQL("insert into Applicant values (:bind1, :bind2, :bind3, :bind4, :bind5)", $alltuples);
+            OCICommit($db_conn);
+        }
+
+        function handleCountRequest() {
+            global $db_conn;
+
+            $result = executePlainSQL("SELECT Count(*) FROM demoTable");
+
+            if (($row = oci_fetch_row($result)) != false) {
+                echo "<br> The number of tuples in demoTable: " . $row[0] . "<br>";
+            }
+        }
+
+        function handleInsertJobRequest() {
+            global $db_conn;
+
+            //Getting the values from user and insert data into the table
+            $tuple = array (
+                ":bind1" => $_POST['InsJobID'],
+                ":bind2" => $_POST['InsJobDeadline'],
+                ":bind3" => $_POST['InsJobRemote'],
+                ":bind4" => $_POST['InsJobPosName'],
+                ":bind5" => $_POST['InsJobStartDate'],
+                ":bind6" => $_POST['InsJobCompID']
+            );
+
+            $alltuples = array (
+                $tuple
+            );
+
+            executeBoundSQL("insert into Job1 values (:bind1, :bind2, :bind3, :bind4, :bind5, :bind6)", $alltuples);
+            $tuple = array (
+                ":bind1" => $_POST['InsJobPosName'],
+                ":bind2" => $_POST['InsJobCatagory']
+            );
+
+            $alltuples = array (
+                $tuple
+            );
+
+            executeBoundSQL("insert into Job2 values (:bind1, :bind2) ", $alltuples);
+            OCICommit($db_conn);
+        }
+
+    
+        // HANDLE ALL POST ROUTES
+	// A better coding practice is to have one method that reroutes your requests accordingly. It will make it easier to add/remove functionality.
+        function handlePOSTRequest() {
+            if (connectToDB()) {
+                if (array_key_exists('resetTablesRequest', $_POST)) {
+                    handleResetRequest();
+                } else if (array_key_exists('updateQueryRequest', $_POST)) {
+                    handleUpdateApplicantRequest();
+                } else if (array_key_exists('insertQueryRequest', $_POST)) {
+                    handleInsertApplicantRequest();
+                } else if (array_key_exists('insertJobRequest', $_POST)) {
+                    handleInsertJobRequest();
+                } 
+
+                disconnectFromDB();
+            }
+        }
+
+        // HANDLE ALL GET ROUTES
+	// A better coding practice is to have one method that reroutes your requests accordingly. It will make it easier to add/remove functionality.
+        function handleGETRequest() {
+            if (connectToDB()) {
+                if (array_key_exists('countTuples', $_GET)) {
+                    handleCountRequest();
+                }
+
+                disconnectFromDB();
+            }
+        }
+
+		if (isset($_POST['reset']) || isset($_POST['updateAccountInfoSubmit']) || isset($_POST['insertSubmit']) || isset($_POST['DeleteSubmit'])) {
+            handlePOSTRequest();
+        } else if (isset($_GET['countTupleRequest'])) {
+            handleGETRequest();
+        }
+		?>
+	</body>
+</html>
